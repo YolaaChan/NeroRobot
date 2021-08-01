81416f6241dc803df0a845b8d0fdf6e4439f30b2
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-01 16:03:39 +0700

    Update __tools.py

--- LaylaRobot/modules/__tools.py
+++ LaylaRobot/modules/__tools.py
@@ -1,10 +1,9 @@
-__mod_name__ = "◎Tools"
+__mod_name__ = "Tools"
 
 __help__ = """
 *Fake Info*
  ❍ /fakegen*:* Generates Fake Information
  ❍ /picgen*:* Generate a fake pic 
-
 *Date-time-Weather*
  ❍ /time <country code>*:* Gives information about a timezone.
  ❍ /weather <city>*:* Get weather info in a particular place.
@@ -13,7 +12,6 @@ __help__ = """
  
 *Quotly*
 - /q : reply any message with this command to create a sticker.
-
 *Converts*
  ❍ /encrypt*:* Encrypts The Given Text
  ❍ /decrypt*:* Decrypts Previously Ecrypted Text
