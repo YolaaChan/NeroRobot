a3d8f8626ad2e85b2a82b661240b27089f184496
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-01 13:37:26 +0700

    Update dbfunc.py

--- LaylaRobot/utils/dbfunc.py
+++ LaylaRobot/utils/dbfunc.py
@@ -3,7 +3,8 @@ from typing import Dict, List, Union
 
 
 coupledb = db.couple
-
+karmadb = db.karma
+nsfwdb = db.nsfw
 
 # Couple Chooser
 
@@ -36,3 +37,48 @@ async def save_couple(chat_id: int, date: str, couple: dict):
         },
         upsert=True
     )
+    
+ # Karma Database
+
+karmadb = db.karma2
+
+
+async def is_karma_on(chat_id: int) -> bool:
+    chat = karmadb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def karma_on(chat_id: int):
+    is_karma = await is_karma_on(chat_id)
+    if is_karma:
+        return
+    return await karmadb.insert_one({"chat_id": chat_id})
+
+
+async def karma_off(chat_id: int):
+    is_karma = await is_karma_on(chat_id)
+    if not is_karma:
+        return
+    return await karmadb.delete_one({"chat_id": chat_id})
+##
+
+async def is_nsfw_on(chat_id: int) -> bool:
+    chat = await nsfwdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
+
+async def nsfw_on(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if is_nsfw:
+        return
+    return await nsfwdb.delete_one({"chat_id": chat_id})
+
+
+async def nsfw_off(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if not is_nsfw:
+        return
+    return await nsfwdb.insert_one({"chat_id": chat_id})
